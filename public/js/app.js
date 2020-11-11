63d7e496b24c554f91af3961034739e262314479
Andrew Weinstein <andrew@altvia.com>
2020-11-10 21:34:56 -0700

    wire up forecast with form and UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,41 @@
-console.log('loaded static JS');
+const getForecast = (address) => {
+  const safeAddress = encodeURIComponent(address);
+  const locationPara = document.getElementById('location');
+  const forecastPara = document.getElementById('forecast');
+  locationPara.innerText = 'Searching...';
+  forecastPara.innerText = '';
+
+  fetch(`/weather?address=${safeAddress}`)
+    .then((resp) => {
+      resp
+        .json()
+        .then((data) => {
+          if (data.error) {
+            console.error('Error fetching weather:', data.error);
+            locationPara.innerText = 'Unable to retrieve weather';
+            forecastPara.innerText = '';
+          } else {
+            const { address, location, forecast } = data;
+            locationPara.innerText = location;
+            forecastPara.innerText = forecast;
+          }
+        })
+        .catch((err) => {
+          console.error('Error fetching weather:', err);
+          locationPara.innerText = 'Unable to retrieve weather';
+          forecastPara.innerText = '';
+        });
+    })
+    .catch((err) => {
+      console.error('Error fetching weather:', err);
+      locationPara.innerText = 'Unable to retrieve weather';
+      forecastPara.innerText = '';
+    });
+};
+
+const weatherForm = document.querySelector('form');
+const searchInput = document.getElementById('search-input');
+weatherForm.addEventListener('submit', (e, something) => {
+  e.preventDefault();
+  getForecast(searchInput.value);
+});
